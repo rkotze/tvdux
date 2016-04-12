ca17952375ffdcafde2b8a8ee330c3bb3892f3b9
Richard Kotze <rkotze@dctfh.com>
2016-04-12 23:24:23 +0000

    Added first test from top list view. Should see a list of shows.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,12 @@
 import should from 'should';
+import { mount } from 'enzyme';
 
-describe('Truth is', () => {
-  it('out there?', () => {
-    (1 + 1).should.equal(2);
-  });
+describe('Given a web user navigates to TVdux', () => {
+  context('When they arrive on the home page', () => {
+    it('Then they should see a list of tv shows', () => {
+      let app = mount(<TvDux />);
+      let showListTotal = app.find('.show-list').length;
+      showListTotal.should.equal(1);
+    });
+  })
 });
