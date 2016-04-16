22b81d3eda70289587ab3c06380a0f53c7c4e586
Richard Kotze <rkotze@dctfh.com>
2016-04-16 12:59:25 +0000

    Added provider wrapper to app entry.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import TvDuxList from './tvdux-app';
+import { TvDux } from './tvdux-app';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-render(<TvDuxList />, document.getElementById('tvdux-app'));
+let store = createStore(() => '');
+
+render(
+	<Provider store={store}>
+  	<TvDux />
+  </Provider>, 
+  document.getElementById('tvdux-app'));
