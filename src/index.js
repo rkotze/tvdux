751dd2520dc9eef4881aef5abed75221b8baebb7
Richard Kotze <rkotze@dctfh.com>
2016-04-24 00:11:48 +0000

    Wired up reducers to store and added thunk middleware.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { TvDux } from './tvdux-app';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-let store = createStore(() => '');
+let store = createStore(reducers, applyMiddleware(thunk));
 
 render(
 	<Provider store={store}>
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import fetchScheduleReducer from './fetch-schedule/fetch-schedule.reducer';
+
+export default combineReducers({
+  fetchScheduleReducer
+});
